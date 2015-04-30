0b5a3900a0d73ed5e1bf4b43fb6743035918ed3e
tianxie2002 <tianxie2002@163.com>
2015-04-30 16:24:44 +0800

    做了一些简单的修改 提交试试 看能不能提交上去

--- WebSite/AdminWeb/AdminLogin.php
+++ WebSite/AdminWeb/AdminLogin.php
@@ -4,4 +4,16 @@
  * User: Aaron
  * Date: 15/4/28
  * Time: 下午4:13
- */ 
+ */
+
+$username = $_GET["username"];
+$password = $_GET["password"];
+
+$json = array();
+
+if($username == "admin" && $password == "admin"){
+    $json["success"] = '1';
+}else{
+    $json["success"] = '0';
+}
+echo json_encode($json);
