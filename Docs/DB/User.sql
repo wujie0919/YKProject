30cf4be02717e41428c45550e5c610abe9fae891
武杰 <15011390281@163.com>
2015-04-30 10:37:28 +0800

    sql describe

---
+++ Docs/DB/User.sql
@@ -0,0 +1,12 @@
+/*
+ 字段说明
+1、userId 用户id，用时间生成
+2、nickName 用户昵称
+3、CreateDate 创建账户时间
+4、token 手机token 用来存储
+5、password 密码
+6、userStatus 账号状态 0：正常 1、被封
+8、from 0、微博 1、QQ
+9、iden 第三方的唯一标识
+预留其他字段
+ */
---
+++ Docs/DB/video.sql
@@ -0,0 +1,10 @@
+/*
+1、videoId 视频Id
+2、videoAddress 上传视频的地址，第一期只选择直辖市、市区、后期做经纬度
+3、videoLength 视频长度
+4、videowner 视频拥有者
+5、videoType 分类
+6、uploadDate 上传时间
+7、videoDes 视频描述
+8、videoStatus 视频状态
+ */
