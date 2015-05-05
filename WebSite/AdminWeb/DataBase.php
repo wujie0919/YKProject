64189168761bfb403325b64cc95b6d29c2175239
tianxie2002 <tianxie2002@163.com>
2015-05-05 09:59:23 +0800

    修改了 一些操作 还没修改完

--- WebSite/AdminWeb/DataBase.php
+++ WebSite/AdminWeb/DataBase.php
@@ -6,35 +6,60 @@
  * Time: 下午5:05
  */
 
-require ("./Config/WebConfig.php");
+require_once ("./Config/WebConfig.php");
 
-class db{
 
-    function fun(){
-        global $db_username,$db_password;
-        echo "数据库用户名：".$db_username;
-        echo "数据库密码：".$db_password;
-    }
+define(DB_HOST,'localhost');
+define(DB_USER,'root');
+define(DB_PASS,'root');
+define(DB_DATABASENAME,'test');
 
-    /**
-     * @param $databaseName
-     * @param $db_password
-     * @return string
-     */
-    public  function queryTab($databaseName, $db_password)
-    {
-        $myconn = mysql_connect("localhost", $db_username, $db_password);
-        mysql_select_db("test", $myconn);
-        mysql_query("set names 'gbk'"); // //这就是指定数据库字符集，一般放在连接数据库后面就系了
-        $strSql = "select * from " . $databaseName;
-        $result = mysql_query($strSql, $myconn);
-        $obj = array();
-        while ($row = mysql_fetch_array($result)) {
-            $obj[$i] = $list;
-            $i++;
-        }
-        mysql_close($myconn);
-        return json_encode($obj);
-    }
 
-}
+//$db_password = "root";
+//$myconn = mysql_connect("localhost", $db_username, $db_password);
+//mysql_select_db("my_db", $myconn);
+//mysql_query("set names 'gbk'"); // //这就是指定数据库字符集，一般放在连接数据库后面就系了
+//$strSql = "select * from YK_User" ;
+//$result = mysql_query($strSql, $myconn);
+//$obj = array();
+//while ($row = mysql_fetch_array($result)) {
+//    $obj[$i] = $list;
+//    $i++;
+//}
+//mysql_close($myconn);
+//if($obj == null)
+//    echo "null";
+//else
+//    echo json_encode($obj);
+
+
+//class db{
+//
+//    function fun(){
+//        global $db_username,$db_password;
+//        echo "数据库用户名：".$db_username;
+//        echo "数据库密码：".$db_password;
+//    }
+//
+//    /**
+//     * @param $databaseName
+//     * @param $db_password
+//     * @return string
+//     */
+//    public  function queryTab($databaseName)
+//    {
+//        $myconn = mysql_connect("localhost", $db_username, $db_password);
+//        mysql_select_db("test", $myconn);
+//        mysql_query("set names 'gbk'"); // //这就是指定数据库字符集，一般放在连接数据库后面就系了
+//        $strSql = "select * from " . $databaseName;
+//        $result = mysql_query($strSql, $myconn);
+//        $obj = array();
+//        while ($row = mysql_fetch_array($result)) {
+//            $obj[$i] = $list;
+//            $i++;
+//        }
+//        mysql_close($myconn);
+//        return json_encode($obj);
+//    }
+//
+//}
