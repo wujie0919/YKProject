4742ca5c72b288b4e538fa9238ee838bf80494b1
tianxie2002 <tianxie2002@163.com>
2015-05-05 11:27:28 +0800

    增删改查demo代码

--- WebSite/AdminWeb/DataBase.php
+++ WebSite/AdminWeb/DataBase.php
@@ -6,60 +6,54 @@
  * Time: 下午5:05
  */
 
-require_once ("./Config/WebConfig.php");
+require_once ("../Config/WebConfig.php");
 
-
-define(DB_HOST,'localhost');
-define(DB_USER,'root');
-define(DB_PASS,'root');
+define(DB_HOST,'localhost:3306');
+define(DB_USER,'user');
+define(DB_PASS,'123');
 define(DB_DATABASENAME,'test');
+define(DB_TABLENAME,'student');
 
+$myconn = mysql_connect(DB_HOST, DB_USER, DB_PASS);
+if(!$myconn){
+    die("connect failed" . mysql_error());
+}else{
+//             echo "success";
+}
+mysql_select_db(DB_DATABASENAME, $myconn);
 
-//$db_password = "root";
-//$myconn = mysql_connect("localhost", $db_username, $db_password);
-//mysql_select_db("my_db", $myconn);
 //mysql_query("set names 'gbk'"); // //这就是指定数据库字符集，一般放在连接数据库后面就系了
-//$strSql = "select * from YK_User" ;
+//$strSql = "select * from " . DB_TABLENAME;
 //$result = mysql_query($strSql, $myconn);
 //$obj = array();
-//while ($row = mysql_fetch_array($result)) {
-//    $obj[$i] = $list;
-//    $i++;
+//while($row = mysql_fetch_assoc($result)){
+//    $obj[] = $row;
+//}
+//echo json_encode($obj);
+
+// insert
+//$sql = "insert into ".DB_TABLENAME." (name,sid,sex,age)values('lipan1','154','1','26')";
+//if(mysql_query($sql,$myconn)){
+//    echo "success";
+//}else{
+//    die('Error: ' . mysql_error());
 //}
 //mysql_close($myconn);
-//if($obj == null)
-//    echo "null";
-//else
-//    echo json_encode($obj);
 
-
-//class db{
-//
-//    function fun(){
-//        global $db_username,$db_password;
-//        echo "数据库用户名：".$db_username;
-//        echo "数据库密码：".$db_password;
-//    }
-//
-//    /**
-//     * @param $databaseName
-//     * @param $db_password
-//     * @return string
-//     */
-//    public  function queryTab($databaseName)
-//    {
-//        $myconn = mysql_connect("localhost", $db_username, $db_password);
-//        mysql_select_db("test", $myconn);
-//        mysql_query("set names 'gbk'"); // //这就是指定数据库字符集，一般放在连接数据库后面就系了
-//        $strSql = "select * from " . $databaseName;
-//        $result = mysql_query($strSql, $myconn);
-//        $obj = array();
-//        while ($row = mysql_fetch_array($result)) {
-//            $obj[$i] = $list;
-//            $i++;
-//        }
-//        mysql_close($myconn);
-//        return json_encode($obj);
-//    }
-//
+// relpace
+//$sql = "update ".DB_TABLENAME." set name = '李攀1' where sid = '12345'";
+//if(mysql_query($sql,$myconn)){
+//    echo "success";
+//}else{
+//    die('Error: ' . mysql_error());
 //}
+//mysql_close($myconn);
+
+// delete
+$sql = "delete from ".DB_TABLENAME." where sid = '12345'";
+if(mysql_query($sql,$myconn)){
+    echo "success";
+}else{
+    die('Error: ' . mysql_error());
+}
+mysql_close($myconn);
